f422c5bc5a0790a30fa02d1827024ba821237492
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-10 20:33:31 -0400

    Creando base de datos

---
+++ Parcial1-Ap2-Padilla/Migrations/20200611003233_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_Ap2_Padilla.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulo",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<double>(nullable: false),
+                    ValorInventario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulo", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulo");
+        }
+    }
+}
