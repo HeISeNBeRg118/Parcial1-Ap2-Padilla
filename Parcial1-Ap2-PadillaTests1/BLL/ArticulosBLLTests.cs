b88ae3e1f4e414e34a0494a288eacf564ba4f0f1
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-11 00:05:12 -0400

    Empezando Test

---
+++ Parcial1-Ap2-PadillaTests1/BLL/ArticulosBLLTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1_Ap2_PadillaTests1.BLL
+{
+    class ArticulosBLLTests
+    {
+
+    }
+}
---
+++ Parcial1-Ap2-PadillaTests2/BLL/ArticulosBLLTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial1_Ap2_Padilla.BLL;
+using Parcial1_Ap2_Padilla.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1_Ap2_Padilla.BLL.Tests
+{
+    [TestClass()]
+    public class ArticulosBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso = false;
+            Articulos articulo = new Articulos();
+
+            articulo.ProductoId = 0;
+            articulo.Descripcion = "Lapiz";
+            articulo.Existencia = 5;
+            articulo.Costo = 10;
+
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
