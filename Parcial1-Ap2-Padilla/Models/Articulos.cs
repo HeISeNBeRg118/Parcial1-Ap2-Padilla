d46a4f34bc566a629a75da36c8db12410164d504
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-10 20:28:15 -0400

    Terminando entidad

---
+++ Parcial1-Ap2-Padilla/Models/Articulos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Padilla.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public int Existencia { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public double Costo { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public double ValorInventario { get; set; }
+    }
+}
